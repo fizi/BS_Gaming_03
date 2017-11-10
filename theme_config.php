84b8acc9f10df052c1b882b20a2d1d62382326f8
fizi <fizi@wsb.hu>
2017-11-10 21:11:55 +0100

    Left or right manuaera from theme admin.

--- theme_config.php
+++ theme_config.php
@@ -15,7 +15,7 @@ class theme_config implements e_theme_config {
 			'videoposter' => array('title' => LAN_BS_GAMING_03_THEMEPREF_02, 'type'=>'image', 'help'=>''),
 			'videourl' => array('title' => LAN_BS_GAMING_03_THEMEPREF_03, 'type'=>'text', 'writeParms'=>array('size'=>'xxlarge'),'help'=>''),
       'setvideobgonoff' => array('title' => LAN_BS_GAMING_03_THEMEPREF_04, 'type'=>'boolean', 'help'=> LAN_BS_GAMING_03_THEMEPREF_05),
-      'leftorrightmenuarea' => array('title' => LAN_BS_GAMING_03_THEMEPREF_06, 'type'=>'dropdown', 'riteParms'=>array('optArray'=> array('left'=> LAN_BS_GAMING_03_THEMEPREF_08,'right'=> LAN_BS_GAMING_03_THEMEPREF_09)), 'help'=> LAN_BS_GAMING_03_THEMEPREF_07),  
+      'leftorrightmenuarea' => array('title' => LAN_BS_GAMING_03_THEMEPREF_06, 'type'=>'dropdown', 'writeParms'=>array('optArray'=> array('left'=> LAN_BS_GAMING_03_THEMEPREF_08,'right'=> LAN_BS_GAMING_03_THEMEPREF_09)), 'help'=> LAN_BS_GAMING_03_THEMEPREF_07),  
 		);
 		return $fields;
 	}
